a472410ff34596ef69b0e5411d9ac3ec70d021d3
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-10-13 20:08:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone Infinityworl/Advance-Auto-Filter /Advance-Auto-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /Advance-Auto-Filter
 fi
-cd /Auto-search-tamil-bot
+cd /Advance-Auto-Filter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
